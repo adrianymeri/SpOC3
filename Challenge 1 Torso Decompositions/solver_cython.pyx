61d5938a3b26afc1a7be35df5d4a31c8f3bf4466
adrianymeri <adrianymeri33@gmail.com>
2025-08-21 01:08:56 +0200

    Update solver_cython.pyx

--- Challenge 1 Torso Decompositions/solver_cython.pyx
+++ Challenge 1 Torso Decompositions/solver_cython.pyx
@@ -6,11 +6,11 @@ import numpy as np
 cimport numpy as np
 from libc.stdlib cimport rand, RAND_MAX
 
-# Declare C-level types for performance
+# --- C-level Type Definitions ---
 ctypedef np.int64_t INT64_t
 ctypedef np.uint64_t UINT64_t
 
-# Global variables for the worker process
+# --- Worker Globals (set by Python) ---
 cdef int N
 cdef UINT64_t[:] ADJ_BITS
 
@@ -19,6 +19,7 @@ def init_worker_cython(int n_val, np.ndarray[UINT64_t, ndim=1] adj_bits_val):
     N = n_val
     ADJ_BITS = adj_bits_val
 
+# --- Core C-level Functions ---
 cdef int bitcount_cy(UINT64_t x):
     cdef int c = 0
     while x > 0:
@@ -64,49 +65,57 @@ cpdef tuple evaluate_solution_cy(np.ndarray[INT64_t, ndim=1] solution):
             
     return (size, max_width)
 
-cdef void swap_cy(INT64_t[:] perm):
-    cdef int n = perm.shape[0]
-    cdef int i = rand() % n
-    cdef int j = rand() % n
-    cdef INT64_t temp = perm[i]
-    perm[i] = perm[j]
-    perm[j] = temp
-
+# --- C-level Local Search Operators ---
 cdef void inversion_cy(INT64_t[:] perm):
-    cdef int n = perm.shape[0]
-    cdef int a = rand() % n
-    cdef int b = rand() % n
+    cdef int n = perm.shape[0], a = rand() % n, b = rand() % n
     cdef int start, end
-    cdef INT64_t temp
     if a == b: return
-    if a < b:
-        start, end = a, b
-    else:
-        start, end = b, a
+    start, end = (a, b) if a < b else (b, a)
     while start < end:
-        temp = perm[start]
-        perm[start] = perm[end]
-        perm[end] = temp
+        perm[start], perm[end] = perm[end], perm[start]
         start += 1
         end -= 1
 
+cdef void block_move_cy(INT64_t[:] perm):
+    cdef int n = perm.shape[0]
+    cdef int block_size = 2 + (rand() % max(1, n // 50))
+    if n <= block_size: return
+    
+    cdef int start_idx = rand() % (n - block_size)
+    cdef int insert_pos = rand() % (n - block_size)
+    
+    # Use a temporary buffer for the block
+    cdef INT64_t[:] block_buffer = np.empty(block_size, dtype=np.int64)
+    block_buffer[:] = perm[start_idx : start_idx + block_size]
+
+    # Shift elements to close the gap
+    if start_idx < insert_pos:
+        perm[start_idx : insert_pos] = perm[start_idx + block_size : insert_pos + block_size]
+    else:
+        perm[insert_pos + block_size : start_idx + block_size] = perm[insert_pos : start_idx]
+
+    # Insert the block
+    perm[insert_pos : insert_pos + block_size] = block_buffer
+
+# --- Main Local Search Function (Memetic Step) ---
 cpdef np.ndarray[INT64_t, ndim=1] local_search_cy(np.ndarray[INT64_t, ndim=1] solution, int intensity):
     cdef np.ndarray[INT64_t, ndim=1] best_sol = solution.copy()
     cdef tuple best_score = evaluate_solution_cy(best_sol)
     cdef np.ndarray[INT64_t, ndim=1] cand_sol
     cdef tuple cand_score
     cdef int shift, t
+    cdef double r
 
     cdef int i
     for i in range(intensity):
         cand_sol = best_sol.copy()
-        # Choose a neighborhood
-        if rand() < 0.7 * RAND_MAX: # Permutation neighborhood
-            if rand() < 0.5 * RAND_MAX:
-                inversion_cy(cand_sol[:-1])
-            else:
-                swap_cy(cand_sol[:-1])
-        else: # Torso shift neighborhood
+        r = <double>rand() / RAND_MAX
+        
+        if r < 0.33:
+            block_move_cy(cand_sol[:-1])
+        elif r < 0.66:
+            inversion_cy(cand_sol[:-1])
+        else:
             t = cand_sol[-1]
             shift = max(1, N // 20)
             t = max(0, min(N - 1, t + (rand() % (2 * shift + 1)) - shift))
@@ -114,7 +123,6 @@ cpdef np.ndarray[INT64_t, ndim=1] local_search_cy(np.ndarray[INT64_t, ndim=1] so
 
         cand_score = evaluate_solution_cy(cand_sol)
 
-        # Dominance check
         if (cand_score[0] > best_score[0]) or \
            (cand_score[0] == best_score[0] and cand_score[1] < best_score[1]):
             best_sol = cand_sol
