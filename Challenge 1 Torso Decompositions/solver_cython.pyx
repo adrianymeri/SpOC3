69bd19ce5825ec1f88207caa1a5ded14bd27064f
adrianymeri <adrianymeri33@gmail.com>
2025-08-21 00:58:45 +0200

    Update and rename test.py to solver_cython.pyx

---
+++ Challenge 1 Torso Decompositions/solver_cython.pyx
@@ -0,0 +1,73 @@
+#cython: language_level=3
+#cython: boundscheck=False
+#cython: wraparound=False
+
+import numpy as np
+cimport numpy as np
+
+# Declare C-level types for performance
+ctypedef np.int64_t INT64_t
+ctypedef np.uint64_t UINT64_t
+
+# Global variables for the worker process
+cdef int N
+cdef UINT64_t[:] ADJ_BITS
+
+# This function will be called once per worker to set the globals
+def init_worker_cython(int n_val, np.ndarray[UINT64_t, ndim=1] adj_bits_val):
+    global N, ADJ_BITS
+    N = n_val
+    ADJ_BITS = adj_bits_val
+
+# C-level bitcount function for maximum speed
+cdef int bitcount_cy(UINT64_t x):
+    cdef int c = 0
+    while x > 0:
+        x &= x - 1
+        c += 1
+    return c
+
+# This is the C-level compiled evaluator. It will be extremely fast.
+cpdef tuple evaluate_solution_cy(np.ndarray[INT64_t, ndim=1] solution):
+    cdef int t = solution[-1]
+    cdef INT64_t[:] perm = solution[:-1]
+    cdef int size = N - t
+    if size <= 0:
+        return (0, 999)
+
+    cdef UINT64_t[:] suffix_mask = np.zeros(N, dtype=np.uint64)
+    cdef UINT64_t curr_mask = 0
+    cdef int i, u, v, out_deg, max_width
+    cdef UINT64_t succ, s, v_bit, temp_v_bit
+
+    for i in range(N - 1, -1, -1):
+        suffix_mask[i] = curr_mask
+        curr_mask |= (1 << perm[i])
+
+    cdef UINT64_t[:] temp = ADJ_BITS.copy()
+    max_width = 0
+
+    for i in range(N):
+        u = perm[i]
+        succ = temp[u] & suffix_mask[i]
+        out_deg = bitcount_cy(succ)
+        if out_deg > max_width:
+            max_width = out_deg
+
+        if succ == 0:
+            continue
+
+        s = succ
+        while s > 0:
+            v_bit = s & -s
+            s ^= v_bit
+
+            v = 0
+            temp_v_bit = v_bit
+            while temp_v_bit > 1:
+                temp_v_bit >>= 1
+                v += 1
+
+            temp[v] |= (succ ^ v_bit)
+
+    return (size, max_width)
