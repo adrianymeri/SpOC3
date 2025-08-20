482c346d93da505ef4d7f2af5669dd87d7f09599
adrianymeri <adrianymeri33@gmail.com>
2025-08-21 01:43:35 +0200

    Update setup.py

--- Challenge 1 Torso Decompositions/setup.py
+++ Challenge 1 Torso Decompositions/setup.py
@@ -2,9 +2,7 @@ from setuptools import setup
 from Cython.Build import cythonize
 import numpy
 
-def build():
-    setup(
-        ext_modules=cythonize("solver_cython.pyx", quiet=True),
-        include_dirs=[numpy.get_include()],
-        script_args=['build_ext', '--inplace']
-    )
+setup(
+    ext_modules=cythonize("solver_cython.pyx", quiet=True),
+    include_dirs=[numpy.get_include()]
+)
